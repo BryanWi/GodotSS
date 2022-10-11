a825caef3234309a1ca102af802cb1af2e375d0a
SetMedinaVZ <74511637+SetMedinaVZ@users.noreply.github.com>
2022-10-10 21:25:03 -0500

    1erNivel
    Falta cambiar monedas

---
+++ Juego/src/NPC.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var interactable = false
+const DIALOG = preload("res://src/DialogueBox.tscn")
+
+func _physics_process(delta):
+	$AnimatedSprite.play()
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		$Label.visible = true
+		interactable = true
+
+
+func _on_Area2D_body_exited(body):
+	if body.name == "Player":
+		$Label.visible = false
+		interactable = false
+
+func _input(event):
+	if Input.is_key_pressed(KEY_E) and interactable == true:
+		$Label.visible = false
+		var dialog = DIALOG.instance()
+		get_parent().add_child(dialog)
+		
